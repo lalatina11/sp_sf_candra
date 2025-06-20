5b801a00a1ae8bc1a9182771e8965555063bdd5a
lalatina11 <rahmadancandra@gmail.com>
2025-06-20 15:04:57 +0700

    chore: ui dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,4 +1,5 @@
 import { Metadata } from "next";
+import Link from "next/link";
 
 export const metadata: Metadata = {
   title: "Project Management | Login",
@@ -6,7 +7,51 @@ export const metadata: Metadata = {
 };
 
 const Page = () => {
-  return <div>Page</div>;
+  return (
+    <div className="h-screen flex flex-col">
+      <header className="border-b border-zinc-400/50 px-4 py-2 flex items-center justify-between">
+        <h1 className="text-2xl font-semibold">Project Management Dashboard</h1>
+        <nav className="flex gap-4">
+          <Link
+            href="/dashboard/projects"
+            className="text-blue-700 dark:text-blue-400 hover:underline"
+          >
+            Projects
+          </Link>
+          <Link
+            href="/dashboard/users"
+            className="text-blue-700 dark:text-blue-400 hover:underline"
+          >
+            Users
+          </Link>
+        </nav>
+      </header>
+      <div className="flex-1 flex">
+        <aside className="w-64 border-r border-zinc-400/50 p-4">
+          <h2 className="text-lg font-semibold mb-2">Menu</h2>
+          <ul>
+            <li className="mb-1">
+              <Link
+                href="/dashboard/projects"
+                className="text-blue-700 dark:text-blue-400 hover:underline"
+              >
+                Projects
+              </Link>
+            </li>
+            <li className="mb-1">
+              <Link
+                href="/dashboard/users"
+                className="text-blue-700 dark:text-blue-400 hover:underline"
+              >
+                Users
+              </Link>
+            </li>
+          </ul>
+        </aside>
+        <main className="flex-1 p-4"></main>
+      </div>
+    </div>
+  );
 };
 
 export default Page;
