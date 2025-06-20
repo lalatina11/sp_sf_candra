f5152a58043c6bb4862fbf2da36d642dbf36015d
lalatina11 <rahmadancandra@gmail.com>
2025-06-20 16:17:08 +0700

    api request libs

---
+++ src/lib/apiRequest.ts
@@ -0,0 +1,10 @@
+export const apiRequest = {
+    get: async (url: string) => {
+        const res = await fetch(url, { credentials: "include" })
+        return { res }
+    },
+    post: async (url: string, formBody?: unknown) => {
+        const res = await fetch(url, { method: "POST", headers: { "Content-Type": "application/json" }, credentials: "include", body: formBody ? JSON.stringify(formBody) : null })
+        return { res }
+    }
+}
