5f44a45e3352bc747c697f777df862f075db858c
lalatina11 <rahmadancandra@gmail.com>
2025-06-21 13:07:17 +0700

    feat: add reset selected projectId

--- src/app/utils/stores.ts
+++ src/app/utils/stores.ts
@@ -7,11 +7,13 @@ type UseProjectStoreType = {
     addNewProject: (newProject: UseProjectStoreType['project']) => void
     selectedProjectId: Project['id'] | null
     setSelectedProjectId: (projectID: UseProjectStoreType['selectedProjectId']) => void
+    resetSelectedProjectId: () => void
 }
 
 export const useProjectStore = create<UseProjectStoreType>((set) => ({
     selectedProjectId: null,
     setSelectedProjectId: (projectId) => set({ selectedProjectId: projectId }),
+    resetSelectedProjectId: () => set({ selectedProjectId: null }),
     project: [],
     addNewProject: (newProject) => set({ project: newProject })
 }))
