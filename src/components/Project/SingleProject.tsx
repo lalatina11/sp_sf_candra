e9d6c9f55045530d0c6cbd0461417ad44358f458
lalatina11 <rahmadancandra@gmail.com>
2025-06-21 11:10:24 +0700

    feat: fetch project owner

--- src/components/Project/SingleProject.tsx
+++ src/components/Project/SingleProject.tsx
@@ -4,16 +4,22 @@ import { useProjectStore } from "@/app/utils/stores";
 const SingleProject = () => {
   const { selectedProjectId, project: AllProjects } = useProjectStore();
   return (
-    <main className="flex-1 flex">
+    <main className="flex-1 flex p-4">
       {AllProjects.length && selectedProjectId ? (
         AllProjects.filter((pr) => pr.id === selectedProjectId).map(
-          (project) => <div key={project.id}>Nama Project: {project.name}</div>
+          (project) => <div className="" key={project.id}>
+            <div className="flex flex-col">
+
+            <span>Nama Project: {project.name}</span>
+            <span>Owner Project: {project.owner.email}</span>
+            </div>
+          </div>
         )
-      ) : (
+      ) : AllProjects.length && !selectedProjectId? (
         <span className="flex-1 flex justify-center items-center">
           Belum ada Project yang dipilih
         </span>
-      )}
+      ):<span>Belum ada Project yang anda buat</span>}
     </main>
   );
 };
