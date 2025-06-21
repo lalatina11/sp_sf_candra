858967d5f97e675c731352c22a8c41735bfc2d49
lalatina11 <rahmadancandra@gmail.com>
2025-06-21 10:12:45 +0700

    feat: add project state manager

--- src/app/utils/stores.ts
+++ src/app/utils/stores.ts
@@ -2,12 +2,15 @@ import { Project } from "@/generated/prisma"
 import { create } from "zustand"
 
 type UseProjectStoreType = {
-    project?: Project | null
+    project: Project[]
+    addNewProject: (newProject: UseProjectStoreType['project']) => void
     selectedProjectId: Project['id'] | null
     setSelectedProjectId: (projectID: UseProjectStoreType['selectedProjectId']) => void
 }
 
 export const useProjectStore = create<UseProjectStoreType>((set) => ({
     selectedProjectId: null,
-    setSelectedProjectId: (projectId) => set({ selectedProjectId: projectId })
+    setSelectedProjectId: (projectId) => set({ selectedProjectId: projectId }),
+    project: [],
+    addNewProject: (newProject) => set({ project: newProject })
 }))
