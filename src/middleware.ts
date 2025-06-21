6c9eb3057be6fc30e6352126141131e510487738
lalatina11 <rahmadancandra@gmail.com>
2025-06-21 17:42:42 +0700

    feat: add more protected route

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,7 +6,7 @@ import { NextResponse } from 'next/server'
 export async function middleware(request: NextRequest) {
     const cookie = cookies()
     const token = (await cookie).get("user_token")?.value as string
-    if (request.nextUrl.pathname.startsWith('/dashboard')) {
+    if (request.nextUrl.pathname.startsWith('/dashboard') || request.nextUrl.pathname.startsWith('/projects/*')) {
         if (!token) {
             return NextResponse.redirect(new URL('/login', request.url))
         }
