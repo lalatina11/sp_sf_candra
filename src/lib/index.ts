adf0985e188d6a11bed7ae2449b8a91790cd53a0
lalatina11 <rahmadancandra@gmail.com>
2025-06-21 17:48:13 +0700

    feat: gettingTaskData for fething data helper

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -4,3 +4,8 @@ export const gettingMembershipUserData = (projectData: ProjectWithUserAndMembers
     const memberships = projectData.flatMap(proj => proj.memberships)
     return memberships
 }
+
+export const gettingTaskData = (projectData: ProjectWithUserAndMemberships[]) => {
+    const tasks = projectData.flatMap(proj => proj.tasks)
+    return tasks
+}
