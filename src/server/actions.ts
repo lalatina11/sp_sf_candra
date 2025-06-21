68a459755e50addc93aed6ebc1043fd4da93ef6c
lalatina11 <rahmadancandra@gmail.com>
2025-06-21 08:53:52 +0700

    refactor: add token age

--- src/server/actions.ts
+++ src/server/actions.ts
@@ -7,7 +7,7 @@ import { redirect } from "next/navigation"
 export const loggedInUser = async () => {
     const cookie = cookies()
     const userToken = (await cookie).get("user_token")?.value
-    const user = await getUserByToken(userToken)
+    const { user } = await getUserByToken(userToken)
     if (!user) return redirect("/login")
     return { user, userId: user.id }
 }
