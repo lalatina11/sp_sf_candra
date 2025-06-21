7909ecca3b32ec202378ebeb12edc06a40ee0086
lalatina11 <rahmadancandra@gmail.com>
2025-06-21 17:45:09 +0700

    optimize: improve ui looks

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,19 +1,11 @@
-import UserDropDownMenu from "@/components/AuthForm/UserDropDownMenu";
-import { ModeToggle } from "@/components/ModeToggle";
-import Project from "@/components/Project";
-import SingleProject from "@/components/Project/SingleProject";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import prisma from "@/lib/prisma";
 import { loggedInUser } from "@/server/actions";
-import { ProjectWithUserAndMemberships } from "@/types";
-import { Metadata } from "next";
-
-export const metadata: Metadata = {
-  title: "Project Management | Dashboard",
-  description: "Multi-User Project Management App",
-};
+import { ProjectWithUserAndMembershipsCount } from "@/types";
+import Link from "next/link";
 
 const Page = async () => {
-  const { user, userId } = await loggedInUser();
+  const { userId } = await loggedInUser();
   const projects = (await prisma.project.findMany({
     where: {
       OR: [{ ownerId: userId }, { memberships: { some: { userId } } }],
@@ -21,27 +13,33 @@ const Page = async () => {
     include: {
       owner: { select: { email: true } },
       memberships: {
-        include: { user: { select: { email: true } } },
+        include: { user: { select: { _count: true } } },
       },
     },
     orderBy: { createdAt: "desc" },
-  })) as ProjectWithUserAndMemberships[];
-
+  })) as ProjectWithUserAndMembershipsCount[];
+  
   return (
-    <div className="h-screen flex flex-col">
-      <header className="border-b border-zinc-400/50 px-4 py-2 flex items-center justify-between">
-        <h1 className="text-2xl font-semibold">Project Management Dashboard</h1>
-        <nav className="flex gap-4">
-          <ModeToggle />
-          <div className="flex gap-3 items-center">
-            <span>{user.email}</span>
-            <UserDropDownMenu />
-          </div>
-        </nav>
-      </header>
-      <div className="flex-1 flex">
-        <Project projects={projects} />
-        <SingleProject currentUserID={userId} user={user} />
+    <div className="p-4 space-y-4">
+      <h1 className="text-3xl font-semibold">Dashboard</h1>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+        {projects.map((project) => (
+          <Link href={`/projects/${project.id}`} key={project.id}>
+            <Card>
+              <CardHeader>
+                <CardTitle>{project.name}</CardTitle>
+              </CardHeader>
+              <CardContent>
+                <p className="text-sm text-gray-500">
+                  Created by: {project.owner.email}
+                </p>
+                <p className="text-sm text-gray-500">
+                  Members: {project.memberships.length}
+                </p>
+              </CardContent>
+            </Card>
+          </Link>
+        ))}
       </div>
     </div>
   );
