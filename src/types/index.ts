4097bbde45718f2271b268408f278e248e2e8ccf
lalatina11 <rahmadancandra@gmail.com>
2025-06-21 17:49:35 +0700

    refactor and new custom implicite data types: ProjectWithUserAndMemberships, TaskWithProjectInfo

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,6 +1,42 @@
-import { Membership, Project, } from "@/generated/prisma";
+import { Membership, Project, Task, } from "@/generated/prisma";
 
 export type ProjectWithUserAndMemberships = Project & {
-    owner: { email: string };
-    memberships: (Membership & { user: { email: string } })[];
+  owner: { email: string };
+  memberships: (Membership & { user: { email: string } })[];
+  tasks: {
+    project: {
+      _count: {
+        memberships: number;
+        tasks: number;
+      };
+      name: string;
+      owner: { email: string };
+      memberships: (Membership & { user: { email: string } })[];
+    };
+  }[];
+};
+export type ProjectWithUserAndMembershipsCount = Project & {
+  owner: { email: string };
+  memberships: (Membership & { user: { _count: number } })[];
+};
+
+export type TaskWithProjectInfo = Task & {
+  project: {
+    _count: {
+      memberships: number;
+      tasks: number;
+    };
+    name: string;
+    owner: {
+      email: string;
+    };
+    memberships: (Membership & {
+      project: {
+        _count: {
+          memberships: number;
+          tasks: number;
+        };
+      };
+    })[];
+  };
 };
