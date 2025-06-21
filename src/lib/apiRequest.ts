2b31a40c3c0b8dbf1f20601135ec51a87287147c
lalatina11 <rahmadancandra@gmail.com>
2025-06-21 17:44:39 +0700

    feat: add new custom api request

--- src/lib/apiRequest.ts
+++ src/lib/apiRequest.ts
@@ -6,5 +6,13 @@ export const apiRequest = {
     post: async (url: string, formBody?: unknown) => {
         const res = await fetch(url, { method: "POST", headers: { "Content-Type": "application/json" }, credentials: "include", body: formBody ? JSON.stringify(formBody) : null })
         return { res }
+    },
+    update: async (url: string, formBody?: unknown) => {
+        const res = await fetch(url, { method: "PATCH", headers: { "Content-Type": "application/json" }, credentials: "include", body: formBody ? JSON.stringify(formBody) : null })
+        return { res }
+    },
+    delete: async (url: string) => {
+        const res = await fetch(url, { method: "DELETE", headers: { "Content-Type": "application/json" }, credentials: "include", })
+        return { res }
     }
 }
