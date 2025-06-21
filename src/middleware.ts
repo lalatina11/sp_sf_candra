4d1186ca57c5fbb53292f137716d25098a166f0d
lalatina11 <rahmadancandra@gmail.com>
2025-06-21 07:02:28 +0700

    feat: add auth middleware

---
+++ src/middleware.ts
@@ -0,0 +1,20 @@
+import { cookies } from 'next/headers'
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+
+// This function can be marked `async` if using `await` inside
+export async function middleware(request: NextRequest) {
+    const cookie = cookies()
+    const token = (await cookie).get("user_token")
+    if (request.nextUrl.pathname.startsWith('/dashboard')) {
+        if (!token) {
+            return NextResponse.redirect(new URL('/login', request.url))
+        }
+    }
+
+    if (request.nextUrl.pathname.startsWith('/login')||request.nextUrl.pathname.startsWith('/register')) {
+        if (token) {
+            return NextResponse.redirect(new URL('/dashboard', request.url))
+        }
+    }
+}
