6dee488ceea5b85b7c00e106c65c37e630a1af56
lalatina11 <rahmadancandra@gmail.com>
2025-06-21 21:23:12 +0700

    refactor: replace projectdata with taskdata

--- src/app/projects/[id]/page.tsx
+++ src/app/projects/[id]/page.tsx
@@ -1,27 +1,19 @@
 import ProjectDetails from "@/components/Project/ProjectDetails";
 import prisma from "@/lib/prisma";
 import { TaskWithProjectInfo } from "@/types";
+import { notFound } from "next/navigation";
 interface Props {
   params: Promise<{ id: string }>;
 }
+
 const Page = async (props: Props) => {
   const { id } = await props.params;
   const tasks = (await prisma.task.findMany({
     where: { projectId: id },
-    include: {
-      project: {
-        select: {
-          _count: true,
-          name: true,
-          owner: { select: { email: true } },
-          memberships: { include: { project: { select: { _count: true } } } },
-        },
-      },
-    },
   })) as TaskWithProjectInfo[];
   return (
     <main className="flex flex-col gap-6">
-      <ProjectDetails tasks={tasks} />
+      {tasks.length ? <ProjectDetails projectId={id} tasks={tasks} /> : notFound()}
     </main>
   );
 };
