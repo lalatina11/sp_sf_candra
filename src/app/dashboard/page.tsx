1b7db81de0dd20ed477098fbfb919b9c24c9821c
lalatina11 <rahmadancandra@gmail.com>
2025-06-22 09:29:47 +0700

    feat: add validation while user does not have any project

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,3 +1,4 @@
+import { buttonVariants } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import prisma from "@/lib/prisma";
 import { loggedInUser } from "@/server/actions";
@@ -24,29 +25,46 @@ const Page = async () => {
     },
     orderBy: { createdAt: "desc" },
   })) as ProjectWithUserAndMembershipsCount[];
-  
+
   return (
     <div className="p-4 space-y-4">
       <h1 className="text-3xl font-semibold">Dashboard</h1>
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
-        {projects.map((project) => (
-          <Link href={`/projects/${project.id}`} key={project.id}>
-            <Card>
-              <CardHeader>
-                <CardTitle>{project.name}</CardTitle>
-              </CardHeader>
-              <CardContent>
-                <p className="text-sm text-gray-500">
-                  Created by: {project.owner.email}
-                </p>
-                <p className="text-sm text-gray-500">
-                  Members: {project.memberships.length}
-                </p>
-              </CardContent>
-            </Card>
-          </Link>
-        ))}
-      </div>
+      <Link
+        className={"" + buttonVariants({ variant: "default" })}
+        href={"/projects"}
+      >
+        Tambahkan Project
+      </Link>
+      {projects.length ? (
+        projects.map((project) => (
+          <div
+            key={project.id}
+            className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4"
+          >
+            <Link href={`/projects/${project.id}`}>
+              <Card>
+                <CardHeader>
+                  <CardTitle>{project.name}</CardTitle>
+                </CardHeader>
+                <CardContent>
+                  <p className="text-sm text-gray-500">
+                    Created by: {project.owner.email}
+                  </p>
+                  <p className="text-sm text-gray-500">
+                    Members: {project.memberships.length}
+                  </p>
+                </CardContent>
+              </Card>
+            </Link>
+          </div>
+        ))
+      ) : (
+        <div className="flex w-full flex-col gap-3 items-center justify-center">
+          <span className="text-sm text-zinc-500">
+            Anda Belum memiliki Project
+          </span>
+        </div>
+      )}
     </div>
   );
 };
