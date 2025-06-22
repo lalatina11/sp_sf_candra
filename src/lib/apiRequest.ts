7fa341c14f61d5481173c0833ee9b1336c23b83a
lalatina11 <rahmadancandra@gmail.com>
2025-06-22 08:30:26 +0700

    feat: protecting api route

--- src/lib/apiRequest.ts
+++ src/lib/apiRequest.ts
@@ -1,18 +1,18 @@
 export const apiRequest = {
     get: async (url: string) => {
-        const res = await fetch(url, { credentials: "include" })
+        const res = await fetch(url, { credentials: "include", headers: { "Recognition": process.env.NEXT_PUBLIC_SECRET_KEY || "" } })
         return { res }
     },
     post: async (url: string, formBody?: unknown) => {
-        const res = await fetch(url, { method: "POST", headers: { "Content-Type": "application/json" }, credentials: "include", body: formBody ? JSON.stringify(formBody) : null })
+        const res = await fetch(url, { method: "POST", headers: { "Content-Type": "application/json", "Recognition": process.env.NEXT_PUBLIC_SECRET_KEY || "" }, credentials: "include", body: formBody ? JSON.stringify(formBody) : null })
         return { res }
     },
     update: async (url: string, formBody?: unknown) => {
-        const res = await fetch(url, { method: "PATCH", headers: { "Content-Type": "application/json" }, credentials: "include", body: formBody ? JSON.stringify(formBody) : null })
+        const res = await fetch(url, { method: "PATCH", headers: { "Content-Type": "application/json", "Recognition": process.env.NEXT_PUBLIC_SECRET_KEY || "" }, credentials: "include", body: formBody ? JSON.stringify(formBody) : null })
         return { res }
     },
     delete: async (url: string) => {
-        const res = await fetch(url, { method: "DELETE", headers: { "Content-Type": "application/json" }, credentials: "include", })
+        const res = await fetch(url, { method: "DELETE", headers: { "Content-Type": "application/json", "Recognition": process.env.NEXT_PUBLIC_SECRET_KEY || "" }, credentials: "include", })
         return { res }
     }
 }
--- src/middleware.ts
+++ src/middleware.ts
@@ -3,9 +3,10 @@ import type { NextRequest } from 'next/server'
 import { NextResponse } from 'next/server'
 
 // This function can be marked `async` if using `await` inside
-export async function middleware(request: NextRequest) {
+export async function middleware(request: NextRequest,) {
     const cookie = cookies()
     const token = (await cookie).get("user_token")?.value as string
+    const recognition = request.headers.get('Recognition')
     if (request.nextUrl.pathname.startsWith('/dashboard') || request.nextUrl.pathname.startsWith('/projects/*')) {
         if (!token) {
             return NextResponse.redirect(new URL('/login', request.url))
@@ -15,6 +16,11 @@ export async function middleware(request: NextRequest) {
     if (request.nextUrl.pathname.startsWith('/login') || request.nextUrl.pathname.startsWith('/register')) {
         if (token) {
             return NextResponse.redirect(new URL('/dashboard', request.url))
+        }
+    }
+    if (request.nextUrl.pathname.startsWith('/api')) {
+        if (recognition !== process.env.NEXT_PUBLIC_SECRET_KEY) {
+            return NextResponse.json({ error: "Unauthorized access" }, { status: 401 });
         }
     }
 }
