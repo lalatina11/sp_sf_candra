3e130bb1da0606bc7b0d514512dd54b518c479da
lalatina11 <rahmadancandra@gmail.com>
2025-06-23 07:16:42 +0700

    refactor: simplified code

--- src/components/Project/TaskCard.tsx
+++ src/components/Project/TaskCard.tsx
@@ -1,15 +1,16 @@
 "use client";
 
+import { TaskWithProjectInfo } from "@/types";
 import { useSortable } from "@dnd-kit/sortable";
 import { CSS } from "@dnd-kit/utilities";
-import { TaskWithProjectInfo } from "@/types";
 
 interface Props {
   task: TaskWithProjectInfo;
   status: string;
 }
 
-const TaskCard = ({ task }: Props) => {
+const TaskCard = (props: Props) => {
+  const { task } = props;
   const { attributes, listeners, setNodeRef, transform, transition } =
     useSortable({ id: task.id, data: { status: task.status } });
 
@@ -18,18 +19,21 @@ const TaskCard = ({ task }: Props) => {
     transition,
   };
 
+
+
   return (
     <div
       ref={setNodeRef}
       style={style}
       {...attributes}
       {...listeners}
-      className="bg-white dark:bg-zinc-800 border rounded shadow p-3 mb-2 cursor-grab"
+      className="bg-white dark:bg-zinc-800 border rounded shadow gap-3 p-3 mb-2 cursor-grab relative"
     >
       <p className="font-medium">{task.title}</p>
-      {task.description && (
-        <p className="text-sm text-zinc-500 mt-1">{task.description}</p>
-      )}
+      <p className="text-sm text-zinc-500 mt-1">{task.description}</p>
+      <div className="relative z-10 flex justify-center gap-5 items-center mt-3">
+
+      </div>
     </div>
   );
 };
