2923a7c512cc00673731e5a71d76ce9811ab3b2f
lalatina11 <rahmadancandra@gmail.com>
2025-06-23 08:36:53 +0700

    feat: add api route for editting title and desc in task table

--- src/app/api/tasks/[id]/route.ts
+++ src/app/api/tasks/[id]/route.ts
@@ -5,11 +5,44 @@ import { NextRequest, NextResponse } from "next/server";
 export const PATCH = async (req: NextRequest, { params }: { params: Promise<{ id: string }> }) => {
     try {
         const { id } = await params
-        const { status } = await req.json() as Task
-        const updatedTask = await prisma.task.update({ where: { id }, data: { status } })
+        const task = await prisma.task.findUnique({ where: { id } });
+        if (!task) {
+            throw new Error("Task not found!");
+        }
+        const { status, title, description } = await req.json() as Task
+        const updatedTask = await prisma.task.update({
+            where: { id },
+            data: {
+                status: status ?? task.status,
+                title: title ?? task.title,
+                description: description ?? task.description,
+            },
+        });
         return NextResponse.json({ message: "OK", data: updatedTask, error: false }, { status: 200 })
     } catch (error) {
         console.error(error)
         return NextResponse.json({ message: (error as Error).message, data: null, error: true }, { status: 400 })
     }
 }
+
+
+export const DELETE = async (req: NextRequest, { params }: { params: Promise<{ id: string }> }) => {
+    try {
+        const { id } = await params
+        if (!id) {
+            throw new Error("ID tugas diperlukan!");
+        }
+        const task = await prisma.task.findUnique({ where: { id } });
+        if (!task) {
+            throw new Error("Tugas tidak ditemukan!");
+        }
+
+        await prisma.task.delete({ where: { id } })
+        return NextResponse.json({ message: "OK", error: false }, { status: 200 })
+    } catch (error) {
+        console.error(error)
+        return NextResponse.json({ message: (error as Error).message, error: true }, { status: 400 })
+    }
+}
+
+
